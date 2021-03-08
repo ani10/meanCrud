280fe506ceb9bddbaed868492d52061ffcdeb2c6
Anirban Sinha <asinha@inmbp015.local>
2021-03-08 07:35:56 +0530

    server Accepts urlencoded data

--- meanCrudBackend/app.js
+++ meanCrudBackend/app.js
@@ -7,7 +7,7 @@ const port= 4201;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", 
